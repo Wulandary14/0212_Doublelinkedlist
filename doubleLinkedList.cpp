fbf029f2368ea282fa8197a2df16a77747d43fc5
Wulandary14 <Moonywulan14@gmail.com>
2026-04-28 16:33:53 +0700

    void revtraverse

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -133,7 +133,7 @@ public:
         if (START == NULL)
         {
             cout << "\nList is empty" << endl;
-            reurn;
+            return;
         }
 
         //step 1: mark first node as currentNode
@@ -153,5 +153,34 @@ public:
         }
     }
 
-    
+void revtraverse()
+{
+    if (START == NULL)
+    {
+        cout << "\nList is empty" << endl;
+        return;
+    }
+
+    //step 1: Move to last node
+    Node *currentNode = START;
+    int 1 = 0;
+    while (currentNode->next != NULL)
+    {
+        currentNode = currentNode->next;
+        i++;
+    }
+
+    //step 2: Traverse backward
+    cout << "\nRecor is descending order of roll number are :\n";
+    while (currentNode != NULL)
+    {
+        cout << i _ 1 << ". " << currentNode->noMhs << " " << endl;
+
+    //step 3: Move to previous node
+    currentNode = currentNode->prev;
+    i--;
+    }
+}
+
+void serachData 
 }
